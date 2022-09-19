f4965ea05f83a12e38a32060b731d6b69fd12efa
erdemburak <burakerdem6@hotmail.com>
2022-09-19 13:14:05 +0300

    Kısayollar

---
+++ src/main/java/com/burakerdem/utils/IntellijShortCut.java
@@ -0,0 +1,10 @@
+package com.burakerdem.utils;
+
+public class IntellijShortCut {
+
+//    Ctrl+Alt+S --> Settings
+//    Alt+Ins --> Constructor, toString, getter-setter
+//    Ctrl+/ --> Single Comment
+//    Ctrl+Shift+/ --> Multiple Comment
+
+}
