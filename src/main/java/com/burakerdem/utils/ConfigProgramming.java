02ad5ae3f2465ba73395031af4d3cde2f5667452
erdemburak <burakerdem6@hotmail.com>
2022-09-19 13:24:59 +0300

    DevTools Aktif Etmek

---
+++ src/main/java/com/burakerdem/utils/ConfigProgramming.java
@@ -0,0 +1,9 @@
+package com.burakerdem.utils;
+
+public class ConfigProgramming {
+
+    // DevTools --> LiveReload
+    //              file -> settings -> compiler -> build project automatically (OK clicked)(Ctrl+Alt+S)
+    //              file -> Advanced Settings -> Allow Auto-make to start even (OK clicked)
+    //
+}
