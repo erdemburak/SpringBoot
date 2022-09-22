51d73fa0f90baad89b48e4eba79d03e8863d2c1e
erdemburak <burakerdem6@hotmail.com>
2022-09-22 20:23:06 +0300

    @PostConstruct

---
+++ src/main/java/com/burakerdem/bean/PostContructTutorial.java
@@ -0,0 +1,36 @@
+package com.burakerdem.bean;
+
+import com.burakerdem.controller.BeanController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import javax.annotation.PostConstruct;
+import java.util.logging.Logger;
+
+@Controller
+public class PostContructTutorial {
+
+
+    @Autowired
+    Logger LOG;
+
+    //Paramatresiz Constructor
+    // because "this.LOG" is null
+    /*public void BeanController() {
+        LOG.info("Log info çağırıldı");
+    }*/
+
+    // bean henüz başlamadığından null alıyoruz ve burada bağımlılığı enjekte edemiyoruz.
+    // Görevi: Bir bean nesnesi oluşturulduğunda hemen oluşur.
+    @PostConstruct
+    public void init(){
+        LOG.info("Log info çağırıldı");
+    }
+
+
+    public static void main(String[] args) {
+        BeanController beanController = new BeanController();
+        System.out.println(beanController);
+    }
+
+}
---
+++ src/main/java/com/burakerdem/controller/BeanController.java
@@ -0,0 +1,24 @@
+package com.burakerdem.controller;
+
+import com.burakerdem.bean.BeanConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class BeanController {
+
+    @Autowired
+    BeanConfig beanConfig;
+
+    // http://localhost:8080/bean/beanDto
+    @GetMapping("/bean/beanDto") // url
+    @ResponseBody // html olmadan direkt ekranda gösterir
+    public String getBeanDto(){
+        return beanConfig.beanDto()+"";
+    }
+
+
+}
