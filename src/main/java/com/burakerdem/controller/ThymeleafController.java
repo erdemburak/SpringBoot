95d0be63694b0e7b65f89ef521e1c41c82712790
erdemburak <burakerdem6@hotmail.com>
2022-09-28 14:07:47 +0300

    @RequestParam

--- src/main/java/com/burakerdem/controller/ThymeleafController.java
+++ src/main/java/com/burakerdem/controller/ThymeleafController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
@@ -86,18 +87,33 @@ public class ThymeleafController {
 
         return "thymeleaf6";
     }
+
     ////////////////////////////////////////////////////////////////////////////////////
-
+    //@PathVariable
     // http://localhost:8080/thymeleaf7/
+    // http://localhost:8080/thymeleaf7/4
     @GetMapping({"/thymeleaf7", "/thymeleaf7/{id}"})
-    public String getThymeleaf7Model(Model model, @PathVariable(name = "id",required = false) Long id) {
+    public String getThymeleaf7Model(Model model, @PathVariable(name = "id", required = false) Long id) {
 
-        if(id!=null){
+        if (id != null) {
             model.addAttribute("key_model1", "id: " + id);
-        }else {
-            model.addAttribute("key_model1", "id bulunamadı" );
+        } else {
+            model.addAttribute("key_model1", "id bulunamadı");
         }
         return "thymeleaf7";
+    }
+
+    //@RequestParam
+    // http://localhost:8080/thymeleaf8?id=4&name=BurakErdem
+    @GetMapping({"/thymeleaf8"})
+    public String getThymeleaf8Model(Model model, @RequestParam(name = "id", required = false, defaultValue = "0") Long id, @RequestParam(name = "name") String adi) {
+
+        if (id != null) {
+            model.addAttribute("key_model1", "id: " + id + " Adı soyadı: " + adi);
+        } else {
+            model.addAttribute("key_model1", "id bulunamadı");
+        }
+        return "thymeleaf8";
     }
 
 }
